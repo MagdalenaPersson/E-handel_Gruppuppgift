0fe1cf666735cc412a245826381889c0035b0198
Daniel <danielrangmyr00@gmail.com>
2026-04-09 11:55:46 +0200

    Skapade branch+la in variabler

--- variabler.R
+++ variabler.R
@@ -1,9 +1,9 @@
 library(tidyverse)
 
-
 orders_clean <- orders_clean %>% 
-  mutate(sales_amount = quantity * unit_price,
-         discount_amount = (unit_price - discounted_price) * quantity,
-         avg_price_after_discount = sales_amount / quantity
-         )
-
+  mutate(
+    discounted_price = unit_price * (1 - discount_pct),
+    sales_amount = quantity * unit_price,
+    discount_amount = (unit_price - discounted_price) * quantity,
+    avg_price_after_discount = discounted_price
+  )
